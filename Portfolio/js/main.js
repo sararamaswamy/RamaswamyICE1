60f957649bc71abd894541cafd24ca3d6d5c20b7
Sara Ramaswamy <sararama@umich.edu>
2018-04-04 20:41:24 -0400

    big commit

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,4 +1,38 @@
-function myEnterFunction() {
-    document.getElementById("demo2").innerHTML = x+=1;
-}
+$(document).ready(() => {
 
+
+	// $("image-container").mouseover(function() {
+	// 	$(".image-container").attr("src", "../Portfolio/assets/bluebackground.png");
+	// });
+
+	$(".image-container").on('focus mouseover', function () {
+		$(".image-container").attr("src", "../Portfolio/assets/bluebackground.png");
+	
+	  // $(this).attr('src', $(this).data("hover"));
+	  // $("image-container").text("What I did");
+	})
+
+
+	$(".image-container").on('focus mouseout', function () {
+		$(".image-container").attr("src", "../Portfolio/assets/prudentialcover.jpg");
+	})
+
+
+
+
+	// $(".image-container").on('blur mouseleave', function () {
+	// 	$(this).attr('src', $(this).data("src"));
+
+	// })
+	// $(".image-container").mouseout(function () {
+	//   $(this).attr('src', $(this).data("src"));
+	// });
+	// $("image-container").mouseover(function() {
+	// 	$(".image-container").attr("src", "../Portfolio/assets/bluebackground.png");
+	// });
+	
+
+ })
+
+
+  
