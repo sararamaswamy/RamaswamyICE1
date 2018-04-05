6cc0342fc80ec0f766dc763b437a64a6d6b7a933
Sara Ramaswamy <sararama@umich.edu>
2018-04-05 14:52:55 -0400

    about me

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -145,13 +145,13 @@ $(document).ready(() => {
 	 $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+        $('.eventhidden').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_event = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it in */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window > bottom_of_event ){
                 
                 $(this).animate({'opacity':'1'},1500);
                     
