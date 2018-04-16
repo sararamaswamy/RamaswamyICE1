aeb94bac0bfc9b0e196d2f70d5b0a8923a0d3e46
Sara Ramaswamy <sararama@umich.edu>
2018-04-16 14:36:36 -0400

    exam2

--- Exam2/js/main.js
+++ Exam2/js/main.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
 	$("#hide").on('click', function() {
 		$(".colleen").css("visibility", "hidden");
+
+
 		$(".umsi").attr("align", "left");
 
 	})
+
 	$("#show").on('click', function() {
 		$(".colleen").css("visibility", "visible");
-		$(".umsi").attr("align", "right");
+		$(".umsi").attr("align", "justify");
+
 
 	})
+
  });
+
